da9c862bb70a33e97456d19b4f71a4cde311ca78
Leila.L <leila.lababsa@hotmail.fr>
2017-08-24 13:52:15 +0200

    modified json file+ added constructor

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 
-  // var xhttp = new XMLHttpRequest();
-  // xhttp.onreadystatechange = function() {
-  //   if (this.readyState == 4 && this.status == 200) {
-  //      alert(this.responseText);
-  //   }
-  // };
-  // xhttp.open("GET", "humans.txt", true);
-  // xhttp.send();
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       alert(this.responseText);
+    }
+  };
+  xhttp.open("GET", "clients.json", true);
+  xhttp.send();
+
+
+  function liste () {
+    this.getName = function() {
+      for (i=0; i < clients.length; i++) {
+        $("#donnéesClient").append("")
+      }
+    };
+  }
