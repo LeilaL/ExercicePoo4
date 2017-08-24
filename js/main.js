8acf93bdba859f3750b0bc873a6f559291f858b2
Leila.L <leila.lababsa@hotmail.fr>
2017-08-24 11:38:11 +0200

    create json file for clients data

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-       alert(this.responseText);
-    }
-  };
-  xhttp.open("GET", "humans.txt", true);
-  xhttp.send();
+  // var xhttp = new XMLHttpRequest();
+  // xhttp.onreadystatechange = function() {
+  //   if (this.readyState == 4 && this.status == 200) {
+  //      alert(this.responseText);
+  //   }
+  // };
+  // xhttp.open("GET", "humans.txt", true);
+  // xhttp.send();
