03e60ce26f5a0fa2e5c0b25969e04750ec9145a5
Leila.L <leila.lababsa@hotmail.fr>
2017-08-24 16:26:56 +0200

    ameliorate the ajax request

--- js/main.js
+++ js/main.js
@@ -2,23 +2,32 @@
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
+
       clients = JSON.parse(this.responseText);
       infosClient.show();
+      for (i=0; i < document.getElementsByTagName("option").length; i++ ) {
+      document.getElementsByTagName("option")[i].addEventListener("click", function() {
+        $("#donnéesClient").empty();
+        var valeurPourTrier = this.value;
+        infosClient.trier(clients.informations, valeurPourTrier);
+
+      });
     }
-  };
+    }
+  }
+
   xhttp.open("GET", "clients.json", true);
   xhttp.send();
 
 
-  function liste () {
+  var Liste = function() {
     this.show = function() {
       for (i=0; i < clients.informations.length; i++) {
         $("#donnéesClient").append("<tr><td>" + clients.informations[i].id + "</td><td>" + clients.informations[i].prenom + "</td><td>"
         + clients.informations[i].age + "</td><td>" + clients.informations[i].ville + "</td></tr>");
       }
+
     };
-  }
-  var infosClient = new liste();
 
 
   this.trier = function (tab, key) {
@@ -31,3 +40,6 @@
           });
           this.show();
 }
+}
+
+var infosClient = new Liste();
